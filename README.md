8a8e16f5c69f8c3059a5cb1c6643cc20e3718ce6
Bastien Reboulet <bastien.reboulet@gmail.com>
2016-04-21 21:37:20 -0700

    Scaffold + sqlite

---
+++ README.md
@@ -0,0 +1,21 @@
+# Truth Or Dare
+
+An Alexa skill to play Truth or Dare
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pipsi install .
+
+
+# Usage
+
+To use it:
+
+    $ tod --help
+
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+An Alexa skill to play Truth or Dare
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='tod',
+    version='0.0.1',
+    url='https://github.com/breboulet/alexa-truth-or-dare',
+    license='GPLv3',
+    author='Bastien Reboulet',
+    author_email='bastien.reboulet@gmail.com',
+    description='An Alexa skill to play Truth or Dare',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'tod = tod.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
